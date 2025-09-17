613e5fdbe7ae7b51aeaca6bacad3c56373a0388e
abubakkar-js-dev <mdabubakkars182@gmail.com>
2025-09-17 07:05:34 +0600

    fixed roledice button issue

--- src/components/RollDiceBtn.tsx
+++ src/components/RollDiceBtn.tsx
@@ -1,11 +1,13 @@
+import type React from "react";
 import type { JSX } from "react";
 
 type RollDiceBtnProps = {
-    isGameWon: boolean;
+    isGameWon: boolean,
+    buttonRef: React.RefObject<HTMLButtonElement | null>,
     rollDice: () => void
 }
 
-const RollDiceBtn = ({ isGameWon, rollDice, buttonRef }):JSX.Element => {
+const RollDiceBtn = ({ isGameWon, rollDice, buttonRef }: RollDiceBtnProps): JSX.Element => {
   return (
     <button ref={buttonRef} className="roll-dice" onClick={rollDice}>
       {isGameWon ? "New Game" : "Roll"}
